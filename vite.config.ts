d3e588507dafa99c73aba697f31d1b9f97d1ba82
weixiongBirnary <49123453+weixiongBirnary@users.noreply.github.com>
2026-01-09 15:32:13 +0800

    fix: build error

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,9 @@ export default defineConfig({
 	resolve: {
 		alias: {
 		  "@": path.resolve(__dirname, "./src"),
+		  "#minpath": path.resolve(__dirname, "./node_modules/vfile/lib/minpath.browser.js"),
+		  "#minproc": path.resolve(__dirname, "./node_modules/vfile/lib/minproc.browser.js"),
+		  "#minurl": path.resolve(__dirname, "./node_modules/vfile/lib/minurl.browser.js"),
 		},
 	  },
 })
